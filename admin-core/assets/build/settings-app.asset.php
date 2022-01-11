73c27b4126742dedaa288e3991cb89c5d9005795
anpimartinez <kingfunnel.co@gmail.com>
2022-01-11 12:51:52 -0500

    Limpieza

--- admin-core/assets/build/settings-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2d144c066aef988e067d32316ff6a190');
